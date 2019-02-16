62a2d400ae91456cf49b798848cd0e91d0517a2e
ikizey <ikizey@gmail.com>
2019-02-16 22:30:54 +0300

    more buttons in grid

--- learn_reading.py
+++ learn_reading.py
@@ -1,14 +1,30 @@
 from tkinter import *
+from random import shuffle
 
 window = Tk()
 
 window.title = ("Читайка")
-window.geometry('300x200')
-btn = Button(window, text="мама", font=("Arial Bold", 50))
-btn.grid(column=0, row=0)
+window.geometry('550x300')
 
-btn = Button(window, text="папа", font=("Arial Bold", 50))
-btn.grid(column=2, row=0)
+l = ["мама", "папа", "молоко",
+     "мы", "вы", "ты",
+     "каша", "вода", "море",
+     "стол", "стул", "мыло"]
+
+shuffle(l)
+d = {}
+buttons = []
+r, c = 0, 0
+for i, word in enumerate(l):
+    d[i] = word
+    if i % 3 == 0:
+        c = 0
+        r += 1
+    else:
+        c += 1
+    print(i, i%3, c, r)
+    buttons.append(Button(window, text=word, font=("Arial Bold", 50)))
+    buttons[i].grid(column=c, row=r)
 
 
 window.mainloop()
