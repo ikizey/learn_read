3422913ccae76b75617189ce2cc3ae1ff9664ddc
ikizey <ikizey@gmail.com>
2019-02-27 14:29:41 +0300

    +some cooments. +columns variable

--- learn_reading.py
+++ learn_reading.py
@@ -1,6 +1,6 @@
 from random import shuffle
 from tkinter import *
-from playsound import playsound
+from playsound import playsound # i don't like this one.
 
 window = Tk()
 window.title = ("LearnRead")
@@ -14,6 +14,9 @@ x = int((ws/2) - (w/2))
 y = int((hs/2) - (h/2))
 window.geometry(f"{w}x{h}+{x}+{y}")
 
+# word list
+# playsound can't open files with cyrillic letters in it's name, so i have to
+# add mapping to "translit" words. Otherwise just russian words would be on list
 l = [("мама", "mama"), ("папа", "papa"), ("молоко", "moloko"),
      ("мы", "my"), ("вы", "vy"), ("ты", "ty"),
      ("каша", "kasha"), ("вода", "voda"), ("море", "more"),
@@ -32,21 +35,28 @@ l = [("мама", "mama"), ("папа", "papa"), ("молоко", "moloko"),
 
 shuffle(l)
 
+# returns argumentless function. for assigning it to button press event command=
 def play_mp3(mp3):
     def x():
         playsound(mp3)
     return x
 
+#amount of columns in button
+columns = 4
+
 buttons = []
 r, c = 0, 0
+# run through shuffled list, making buttons, assigning functions to them
+# and adding them to grid.
 for i, word in enumerate(l):
-    if i % 4 == 0:
+    if i % columns == 0:
         c = 0
         r += 1
     else:
         c += 1
     ps = play_mp3(f"sounds/{word[1]}.mp3")
-    buttons.append(Button(window, text=f"{i+1}.{word[0]}", font=("Arial Bold", 50), command=ps))
+    buttons.append(Button(window, text=f"{i+1}.{word[0]}"
+                          , font=("Arial Bold", 50), command=ps))
     buttons[i].grid(column=c, row=r)
 
 
